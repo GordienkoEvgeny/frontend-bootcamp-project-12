7c0d404ecda2a0ff14dcff107535c58ca8c25954
GordienkoEvgeny <evgeny.gordienko95@gmail.com>
2023-03-02 17:49:00 +0300

    add files & add bootstrap, linter, Formik, yup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Authorization from './pages/Authorization';
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<Authorization />} />
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,9 @@
+const BuildPage = () => (
+  <>
+    <h3>Page text </h3>
+    <div>
+      Page content: main
+    </div>
+  </>
+);
+export default BuildPage;
